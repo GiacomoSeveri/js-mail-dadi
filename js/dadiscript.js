1e9e99d2d7b7990951cc81933dc65c721b5d4f2f
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-12 16:41:51 +0100

    fixed javascript

--- js/dadiscript.js
+++ js/dadiscript.js
@@ -1,3 +1,11 @@
+// 1 prendo gli elementi
+// 2 aggiungo addeventlistener al btn
+// 3 creo 2 numeri casuali
+// 4 li stampo in pagina
+// 5 verifico quale è il piu grande
+// 6stampo in pagia l'esito della sfida
+
+// 1
 const diceRoll = document.getElementById('dice');
 const cBtn = document.getElementById('btn-casual');
 const dOne = document.getElementById('dice-1');
@@ -7,22 +15,29 @@ const gg = document.getElementById('winner');
 let randomOne = '';
 let randomTwo = '';
 
+// 2
 cBtn.addEventListener('click', function(){
+
+    // 3
     randomOne = Math.floor(Math.random() * 6) + 1;
     // console.log(randomOne);
-    
+
     dOne.innerText = `Il primo numero è ${randomOne}`;
 
+    // 3
     randomTwo = Math.floor(Math.random() * 6) + 1;
     // console.log(randomTwo);
-
     dTwo.innerText = `Il secondo numero è ${randomTwo}`;
 
+    // 4
     if (randomOne < randomTwo) {
+        // 5
         gg.innerText = `Il vincitore è ${randomTwo}, COMPLIMENTI!`;
     } else if (randomOne > randomTwo) {
+        // 5
         gg.innerText = `Il vincitore è ${randomOne}, COMPLIMENTI!`;
     } else if (randomOne === randomTwo) {
+        // 5
         gg.innerText = 'Pareggio, sarà per la prossima.';
     }
     
